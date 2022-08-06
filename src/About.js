d9931ca16869f2d7de381fa19402eb1e2226df4b
jainam03 <jbchheda03@gmail.com>
2022-08-06 13:56:32 +0530

    started working on Abount component

--- src/About.js
+++ src/About.js
@@ -4,7 +4,18 @@ import pic from "./peep (1).png"
 export default function About() {
     return (
         <div>
-            <img src={pic} alt="profile" width="350px" className="my-img" />
+            {<img src={pic} alt="profile" width="350px" className="my-img" />}
+
+            <div id="my-info">
+                <h2>Jainam Chheda</h2>
+                <h3>Frontend developer</h3>
+            </div>
+
+            <div className="buttons">
+                <button id="email-btn">Email</button>
+                <button id="linkedin-btn">Linkedin</button>
+            </div>
+
         </div>
     )
 }
