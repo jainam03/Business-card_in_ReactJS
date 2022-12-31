f20310a5b6eb7cd2e53cc5374331a4648a77f63d
jainam03 <jbchheda03@gmail.com>
2022-12-31 19:49:52 +0530

    redesign footer and font-size

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="footer">
-            <span>
-                <div class="fa fa-twitter" id="icon" ></div>
-                <div class="fa fa-facebook" id="icon" ></div>
-                <div class="fa fa-instagram" id="icon" ></div>
-                <div class="fa fa-github" id="icon" onClick={() => window.open('https://github.com/jainam03/Business-card_in_ReactJS')} ></div>
-            </span>
+
+            <div class="fa fa-twitter fa-lg" id="icon" aria-hidden="true" ></div>
+            <div class="fa fa-facebook fa-lg" id="icon" aria-hidden="true" ></div>
+            <div class="fa fa-instagram fa-lg" id="icon" aria-hidden="true" ></div>
+            <div class="fa fa-github fa-lg" id="icon" aria-hidden="true" onClick={() => window.open('https://github.com/jainam03/Business-card_in_ReactJS')} ></div>
+
         </div>
     )
 }
