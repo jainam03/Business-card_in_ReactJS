1b46d85a7d0113f2f7f0408bcbb96c7f87ab64e6
jainam03 <jbchheda03@gmail.com>
2022-12-31 18:16:16 +0530

    added github repo redirect

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
                 <div class="fa fa-twitter" id="icon" ></div>
                 <div class="fa fa-facebook" id="icon" ></div>
                 <div class="fa fa-instagram" id="icon" ></div>
-                <div class="fa fa-github" id="icon" ></div>
+                <div class="fa fa-github" id="icon" onClick={() => window.open('https://github.com/jainam03/Business-card_in_ReactJS')} ></div>
             </span>
         </div>
     )
